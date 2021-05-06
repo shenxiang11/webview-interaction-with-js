915edcbb54d47cd4a760557d1ec22d63abe6d66b
Shen <863461783@qq.com>
2021-05-06 12:26:46 +0800

    step 4: 完成js 调用 swift

--- webview-interaction-with-js/ViewController.swift
+++ webview-interaction-with-js/ViewController.swift
@@ -11,25 +11,52 @@ import WebKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var webview: WKWebView!
-//    lazy var userContentController = WKUserContentController()
-//    lazy var configuaration: WKWebViewConfiguration = {
-//        let conf = WKWebViewConfiguration()
-//        conf.userContentController = userContentController
-//        return conf
-//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webview.configuration.userContentController.add(self, name: "hahaha")
                                 
-        let htmlFile = try! String(contentsOfFile: Bundle.main.path(forResource: "index", ofType: "html")!)
-        webview.loadHTMLString(htmlFile, baseURL: nil)
+        let htmlFile = Bundle.main.path(forResource: "index", ofType: "html")!
+        webview.loadFileURL(URL(fileURLWithPath: htmlFile), allowingReadAccessTo: URL(fileURLWithPath: htmlFile))
     }
     
     // swift 执行 javascript
     @IBAction func handlePlayBtnTapped(_ sender: UIBarButtonItem) {
-        webview.evaluateJavaScript("call1()") { result, error in
+        webview.evaluateJavaScript("callJavascript()") { result, error in
             print(type(of: result), result)
             print(type(of: error), error)
+        }
+    }
+    
+    func showModal() {
+        let alertController = UIAlertController(title: "恭喜", message: "你通过 js 调用了 swift 来展示一个 alert", preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "确定", style: .default) {_ in
+            self.dismiss(animated: true)
+        }
+        alertController.addAction(confirm)
+        navigationController?.present(alertController, animated: true)
+    }
+    
+    func showAction() {
+        let alertController = UIAlertController(title: "恭喜", message: "你通过 js 调用了 swift 来展示一个action sheet", preferredStyle: .actionSheet)
+        let confirm = UIAlertAction(title: "确定", style: .default) {_ in
+            self.dismiss(animated: true)
+        }
+        alertController.addAction(confirm)
+        navigationController?.present(alertController, animated: true)
+    }
+}
+
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        switch "\(message.body)" {
+        case "showAction":
+            showAction()
+        case "showModal":
+            showModal()
+        default:
+            fatalError("未定义的行为")
         }
     }
 }
