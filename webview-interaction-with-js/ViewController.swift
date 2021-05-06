420fc5ed6a2b66c12acdfa1d2024652c3557e290
Shen <863461783@qq.com>
2021-05-06 11:19:12 +0800

    step3:swift 执行 javascript

--- webview-interaction-with-js/ViewController.swift
+++ webview-interaction-with-js/ViewController.swift
@@ -11,14 +11,26 @@ import WebKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var webview: WKWebView!
+//    lazy var userContentController = WKUserContentController()
+//    lazy var configuaration: WKWebViewConfiguration = {
+//        let conf = WKWebViewConfiguration()
+//        conf.userContentController = userContentController
+//        return conf
+//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                                
         let htmlFile = try! String(contentsOfFile: Bundle.main.path(forResource: "index", ofType: "html")!)
         webview.loadHTMLString(htmlFile, baseURL: nil)
     }
-
-
+    
+    // swift 执行 javascript
+    @IBAction func handlePlayBtnTapped(_ sender: UIBarButtonItem) {
+        webview.evaluateJavaScript("call1()") { result, error in
+            print(type(of: result), result)
+            print(type(of: error), error)
+        }
+    }
 }
 
